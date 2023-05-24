d560ee65d16e923caf9dfdfb74010738169659d4
Kirubel <kirubelgirmay21@gmail.com>
2023-05-24 07:39:45 +0300

    routes started

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { Route,Routes } from 'react-router';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+const Home = () => <h1>Welocme to home</h1>
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      <Route path='/' element={<Home />} >  </Route>
+
+      </Routes>
+    </Router>
+    // <div className="">
+    //   <h1>Hi There</h1>
+    // </div>
   );
 }
 
